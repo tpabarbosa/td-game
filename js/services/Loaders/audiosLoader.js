2a3c07277ae176af0315c066d5d7903cee65879a
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2022-05-02 10:35:13 -0300

    fix audio files

--- js/services/Loaders/audiosLoader.js
+++ js/services/Loaders/audiosLoader.js
@@ -1,72 +1,72 @@
 const audios = {
     musics: [
-        { id: "playing", src: "/audios/443987__adnova__hide-loop.mp3" },
-        { id: "menu", src: "/audios/menu_music.mp3" },
+        { id: "playing", src: "./audios/443987__adnova__hide-loop.mp3" },
+        { id: "menu", src: "./audios/menu_music.mp3" },
     ],
     effects: [{
             id: "coin_released",
-            src: "/audios/213987__fenrirfangs__coin-fall-1.mp3",
+            src: "./audios/213987__fenrirfangs__coin-fall-1.mp3",
         },
         {
             id: "chest_released",
-            src: "/audios/435033__ongaku-dragon__3-table-with-glass-and-keys-hit-and-keys-kick-combi-bonus.mp3",
+            src: "./audios/435033__ongaku-dragon__3-table-with-glass-and-keys-hit-and-keys-kick-combi-bonus.mp3",
         },
         {
             id: "game_paused",
-            src: "/audios/450509__abyeditsound__clockticksound-01.mp3",
+            src: "./audios/450509__abyeditsound__clockticksound-01.mp3",
         },
         {
             id: "hero_shooting_bullet",
-            src: "/audios/522279__filmmakersmanual__bullet-flyby-1.mp3",
+            src: "./audios/522279__filmmakersmanual__bullet-flyby-1.mp3",
         },
         {
             id: "hero_moving",
-            src: "/audios/445635__tilano408__10-steps-grass.mp3",
+            src: "./audios/445635__tilano408__10-steps-grass.mp3",
         },
         {
             id: "wall_beingAttacked",
-            src: "/audios/522401__filmmakersmanual__bullet-concrete-hit-4.mp3",
+            src: "./audios/522401__filmmakersmanual__bullet-concrete-hit-4.mp3",
         },
         {
             id: "skeleton_beingAttacked",
-            src: "/audios/416838__tonsil5__grunt2-death-pain.mp3",
+            src: "./audios/416838__tonsil5__grunt2-death-pain.mp3",
         },
         {
             id: "zombie_beingAttacked",
-            src: "/audios/442601__topschool__ough.mp3",
+            src: "./audios/442601__topschool__ough.mp3",
         },
         {
             id: "mage_beingAttacked",
-            src: "/audios/553285__nettoi__hurt4.mp3",
+            src: "./audios/553285__nettoi__hurt4.mp3",
         },
         {
             id: "mage_shooting_bullet",
-            src: "/audios/522278__filmmakersmanual__bullet-flyby-5.mp3",
+            src: "./audios/522278__filmmakersmanual__bullet-flyby-5.mp3",
         },
         {
             id: "warrior_beingAttacked",
-            src: "/audios/491833__hidrolion__31-quejido-2.mp3",
+            src: "./audios/491833__hidrolion__31-quejido-2.mp3",
         },
 
         {
             id: "life_lost",
-            src: "/audios/416177__johnnyguitar01__bristol-colston-hall-balloon-pop.mp3",
+            src: "./audios/416177__johnnyguitar01__bristol-colston-hall-balloon-pop.mp3",
         },
         {
             id: "end_level_win_condition",
-            src: "/audios/580310__colorscrimsontears__fanfare-2-rpg.mp3",
+            src: "./audios/580310__colorscrimsontears__fanfare-2-rpg.mp3",
         },
         {
             id: "end_level_lost_condition",
-            src: "/audios/368367__thezero__game-over-sound.mp3",
+            src: "./audios/368367__thezero__game-over-sound.mp3",
         },
         {
             id: "end_of_game",
-            src: "/audios/457541__kojiro-miura__sisro-the-winner.mp3",
+            src: "./audios/457541__kojiro-miura__sisro-the-winner.mp3",
         },
         {
             id: "button_clicked",
-            src: "/audios/button_pressed.mp3",
+            src: "./audios/button_pressed.mp3",
         },
     ],
 };
